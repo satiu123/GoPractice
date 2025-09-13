afe806d1005efb77f822e52e7a52c7d47c44fd34
satiu123 <s13658395102@gmail.com>
2025-09-13 16:47:39 +0800

    first commit

---
+++ gee/gee.go
@@ -0,0 +1,42 @@
+package gee
+
+import (
+	"net/http"
+)
+
+type HandlerFunc func(c *Context)
+
+type Engine struct {
+	router *router
+}
+
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	e.router.addRoute(method, pattern, handler)
+}
+
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+func (e *Engine) UPDATE(pattern string, handler HandlerFunc) {
+	e.addRoute("UPDATE", pattern, handler)
+}
+func (e *Engine) DELETE(pattern string, handler HandlerFunc) {
+	e.addRoute("DELETE", pattern, handler)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	e.router.handle(c)
+}
+
+func (e *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, e)
+}
