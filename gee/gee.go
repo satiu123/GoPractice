e0bacabc3ed00f4c04d8f9796753f523a76be3d3
satiu123 <s13658395102@gmail.com>
2025-09-14 14:18:18 +0800

    重构路由组，更新GET和POST方法的实现，调整首页和子路由的处理逻辑

--- gee/gee.go
+++ gee/gee.go
@@ -7,29 +7,50 @@ import (
 type HandlerFunc func(c *Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup //存储所有分组
+}
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc //中间件
+	parent      *RouterGroup  //
+	engine      *Engine       //所有分组共享同一个Engine
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: group.engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	e.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
-
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
+func (group *RouterGroup) PUT(pattern string, handler HandlerFunc) {
+	group.addRoute("PUT", pattern, handler)
 }
-func (e *Engine) PUT(pattern string, handler HandlerFunc) {
-	e.addRoute("PUT", pattern, handler)
-}
-func (e *Engine) DELETE(pattern string, handler HandlerFunc) {
-	e.addRoute("DELETE", pattern, handler)
+func (group *RouterGroup) DELETE(pattern string, handler HandlerFunc) {
+	group.addRoute("DELETE", pattern, handler)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
